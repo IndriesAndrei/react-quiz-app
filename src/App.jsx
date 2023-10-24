4724863b6bb609cb66d3fb52ac21699f40c5bca5
Indries Andrei <indriesandrei@yahoo.co.uk>
2023-10-24 08:58:54 +0300

    Fetching questions from our fake API and install json-server package

--- src/App.jsx
+++ src/App.jsx
@@ -1,10 +1,52 @@
+import { useEffect } from 'react'
 import './App.css'
-import DateCounter from './components/DateCounter'
+import Header from './components/Header'
+import Main from './components/Main'
+import { useReducer } from 'react'
+
+const initialState = {
+  questions: [],
+  // 'loading', 'error', 'ready', 'active', 'finished'
+  status: 'loading'
+}
+
+function reducer(state, action) {
+  switch(action.type) {
+    case 'dataReceived': 
+      return {
+        ...state, 
+        questions: action.payload, 
+        status: 'ready'
+      };
+    case 'dataFailed':
+      return {
+        ...state, 
+        status: 'error'
+      }
+    default:
+      throw new Error('Action unknown')
+  }
+}
 
 export default function App() {
+  const [state, dispatch] = useReducer(reducer, initialState);
+
+  // fetching data from our fake API with json-server package
+  useEffect(function() {
+    fetch('http://localhost:8000/questions')
+      .then((res) => res.json())
+      .then((data) => dispatch({type: 'dataReceived', payload: data})) //sending data to the reducer
+      .catch((err) => dispatch({type: 'dataFailed'}));
+  }, [])
+
   return (
-    <>
-     <DateCounter />
-    </>
+    <div className='app'>
+      <Header />
+
+      <Main>
+        <p>1/15</p>
+        <p>Question?</p>
+      </Main>
+    </div>
   )
 }
